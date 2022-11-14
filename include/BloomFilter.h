816f753b412ed8e69a335d4922e42825f1d020b5
Tianyi Zhang <tonyzhang617@gmail.com>
2022-11-13 23:57:40 -0600

    Bug fix and improvements

--- include/BloomFilter.h
+++ include/BloomFilter.h
@@ -9,11 +9,12 @@ public:
     BloomFilter(uint32_t sz, uint32_t k_, bool disk);
     void insert(uint32_t *hashes);
     bool test(uint32_t *hashes);
+    uint32_t count() const;
     // void release();
 
     uint8_t *bits;
     int file_write;
-
+    uint32_t size;
     uint32_t k;
 };
 
