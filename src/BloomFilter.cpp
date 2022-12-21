c22999011e106b896a0f40ebcac4bd6c0f603fcf
Gaurav Gupta <gg29@terminator5.cs.rice.edu>
2022-12-21 09:53:31 -0600

    memmap disk BF array

--- src/BloomFilter.cpp
+++ src/BloomFilter.cpp
@@ -1,11 +1,19 @@
 #include "BloomFilter.h"
+#include <fcntl.h>
+#include <sys/mman.h>
 
 using namespace std;
 
-BloomFilter::BloomFilter(uint32_t sz, uint32_t k)
-  : bits(new uint8_t[sz >> 3]), k(k) {
-
+BloomFilter::BloomFilter(uint32_t sz, uint32_t k_, bool disk){
+  k =k_;
+  if( disk){
+    file_write = open("bits.dat", O_CREAT | O_RDWR, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+    posix_fallocate(file_write, 0, sz >> 3); // sz >> 3 shd be multiple of 4096
+    bits = reinterpret_cast<uint8_t*>(mmap(NULL, sz >> 3, PROT_WRITE, MAP_SHARED, file_write, 0));
+  }
+  else bits = new uint8_t[sz >> 3];
 }
+
 
 void BloomFilter::insert(uint32_t *hashes) {
   // TODO: hardcode for loop
@@ -22,3 +30,11 @@ bool BloomFilter::test(uint32_t *hashes) {
   }
   return result;
 }
+
+// void BloomFilter::release() {
+//   munmap (bits, sz >> 3);
+//   close (file_write);
+// }
+
+// make sure bits is all zeros
+// https://bertvandenbroucke.netlify.app/2019/12/08/memory-mapping-files/ 
