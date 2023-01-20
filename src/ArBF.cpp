5c876d6fef8d5e1688cfad77ad3e377a3ab04ed9
Gaurav Gupta <gg29@terminator5.cs.rice.edu>
2023-01-20 01:25:28 -0600

    ABF insert done

--- src/ArBF.cpp
+++ src/ArBF.cpp
@@ -22,54 +22,49 @@ ArBF::ArBF(uint32_t N ){
     uint32_t rangeOffset[N];
 }
 
-void ArBF::insert(string folder, const Config config){
+void ArBF::insert(string filellistname, const Config config){
     uint32_t range;
-    uint32_t seen = 0;
-    DIR *dir;
-    struct dirent *ent;
+    uint32_t n = 0;
 
-    if ((dir = opendir (folder.c_str())) != NULL) {
-        while ((ent = readdir (dir)) != NULL && seen<=N) {
-            string fileName(ent->d_name);
-            if (fileName.substr(fileName.length() - 6) == ".fastq") {
-                string fname = ent->d_name;
-                vector<string> sequences = getFastqData("./" + folder+ fname);
-                range = config.rangefactor*config.k*sequences.size();
-                ArBF_array[seen] = new BloomFilter(range, config.k, config.disk, fileName.substr(0,fileName.length() - 6));
+    ifstream filellist(filellistname);
+    string fileName;
+    while (getline(filellist, fileName) && n<=N ) {
+        vector<string> sequences = getFastqData("./data/"+ fileName);
+        range = config.rangefactor*config.k*sequences.size();
+        ArBF_array[n] = new BloomFilter(range, config.k, config.disk, fileName.substr(0,fileName.length() - 6));
 
-                Hasher *hasher[config.numThreads]; // each thread gets its own hasher
-                for (uint32_t i = 0; i < config.numThreads; ++i) {
-                    hasher[i] = config.hashType == Config::MURMUR_HASH
-                    ? static_cast<Hasher*>(new MurmurHasher(static_cast<uint32_t>(config.range), config.k, config.seed))
-                    : (config.hashType == Config::BRUTE_FORCE_FUZZY_HASH
-                        ? static_cast<Hasher*>(new FuzzyHasher(static_cast<uint32_t>(config.range), config.k, config.kMer, config.universalHashRange, config.seed))
-                        : static_cast<Hasher*>(new EfficientFuzzyHasher(static_cast<uint32_t>(config.range), config.k, config.kMer, config.universalHashRange, config.seed)));
-                }
-                // # pragma omp parallel for 
-                for (size_t i = 0; i < sequences.size(); ++i) {
-                    uint32_t hashes[config.k];
-                    uint32_t threadId = 0; // omp_get_thread_num();
-                    hasher[threadId]->setSequence(sequences[i]);
-                    while (hasher[threadId]->hasNext()) {
-                        // t1 = chrono::high_resolution_clock::now();
-                        hasher[threadId]->hash(hashes);
-                        // t2 = chrono::high_resolution_clock::now();
-                        ArBF_array[seen]->insert(hashes);
-                        // t3 = chrono::high_resolution_clock::now();
-                        // hashTimeAccu += chrono::duration_cast<chrono::microseconds>(t2 - t1).count();
-                        // bfTimeAccu += chrono::duration_cast<chrono::microseconds>(t3 - t2).count();
-                        // counter[seen] += 1;
-                    }
-                }
-                seen++;
-                numInsert[seen] = sequences.size();
-                rangeOffset[seen+1] = rangeOffset[seen] + range;
+        Hasher *hasher[config.numThreads]; // each thread gets its own hasher
+        for (uint32_t i = 0; i < config.numThreads; ++i) {
+            hasher[i] = config.hashType == Config::MURMUR_HASH
+            ? static_cast<Hasher*>(new MurmurHasher(static_cast<uint32_t>(config.range), config.k, config.seed))
+            : (config.hashType == Config::BRUTE_FORCE_FUZZY_HASH
+                ? static_cast<Hasher*>(new FuzzyHasher(static_cast<uint32_t>(config.range), config.k, config.kMer, config.universalHashRange, config.seed))
+                : static_cast<Hasher*>(new EfficientFuzzyHasher(static_cast<uint32_t>(config.range), config.k, config.kMer, config.universalHashRange, config.seed)));
+        }
+        // # pragma omp parallel for 
+        for (size_t i = 0; i < sequences.size(); ++i) {
+            uint32_t hashes[config.k];
+            uint32_t threadId = 0; // omp_get_thread_num();
+            hasher[threadId]->setSequence(sequences[i]);
+            while (hasher[threadId]->hasNext()) {
+                // t1 = chrono::high_resolution_clock::now();
+                hasher[threadId]->hash(hashes);
+                // t2 = chrono::high_resolution_clock::now();
+                ArBF_array[n]->insert(hashes);
+                // t3 = chrono::high_resolution_clock::now();
+                // hashTimeAccu += chrono::duration_cast<chrono::microseconds>(t2 - t1).count();
+                // bfTimeAccu += chrono::duration_cast<chrono::microseconds>(t3 - t2).count();
+                // counter[n] += 1;
             }
         }
-        if (seen<N) printf("WARNING: less than %d .fastq files!", N);
-        closedir (dir);
+        n++;
+        numInsert[n] = sequences.size();
+        rangeOffset[n+1] = rangeOffset[n] + range;
+        printf("%s\t", fileName.c_str());
     }
-    else cerr <<"can open the folder: "<< folder<<endl;
+    if (n<N) printf("WARNING: less than %d .fastq files!", N);
+    
+    // else cerr <<"can open the folder: "<< folder<<endl;
 }
 
 
@@ -77,7 +72,7 @@ void ArBF::insert(string folder, const Config config){
 // }
 
 
-// set<int> ArBF::query (string query_key, int len){
+// vector<uint32_t> ArBF::query (string query_key, int len){
 
 // }
 //   set<int> res;
